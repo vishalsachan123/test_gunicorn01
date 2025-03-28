61a21c9fc5866d7523cd366fd5c1541c8a9f8135
Vishal Sachan <129661155+vishalsachan123@users.noreply.github.com>
2025-03-28 20:52:06 +0530

    Update with_gevent.py

--- with_gevent.py
+++ with_gevent.py
@@ -1,3 +1,31 @@
+# from gevent import monkey
+# monkey.patch_all()
+
+# from flask import Flask, jsonify
+# import time
+
+# app = Flask(__name__)
+
+# @app.route('/fast')
+# def fast_route():
+#     return jsonify({'message': 'Fast response hai'})
+
+# @app.route('/slow')
+# def slow_route():
+#     time.sleep(20)  # Simulates a slow response
+#     return jsonify({'message': 'Slow response after 20 sec'})
+
+# if __name__ == '__main__':
+#     # Run with Gevent for high concurrency
+#     from gevent.pywsgi import WSGIServer
+#     http_server = WSGIServer(('0.0.0.0', 5000), app)
+#     print("Gevent WSGI server starting on http://0.0.0.0:5000")
+#     http_server.serve_forever()
+
+#above is also working 
+
+
+# Monkey patching to make standard libraries non-blocking
 from gevent import monkey
 monkey.patch_all()
 
@@ -16,8 +44,5 @@ def slow_route():
     return jsonify({'message': 'Slow response after 20 sec'})
 
 if __name__ == '__main__':
-    # Run with Gevent for high concurrency
-    from gevent.pywsgi import WSGIServer
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    print("Gevent WSGI server starting on http://0.0.0.0:5000")
-    http_server.serve_forever()
+    # For local testing only (optional)
+    app.run(host='0.0.0.0', port=5000)
